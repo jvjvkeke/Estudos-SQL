314c459e8945c7f93689b08e1dd7fba553517a85
Joao VItor Vieira de Souza <41848897+jvjvkeke@users.noreply.github.com>
2026-04-22 22:30:44 -0300

    Criando Database e Table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,16 @@
+create database cadastro
+default character set utf8
+default collate utf8_general_ci;
 
+
+create table pessoas(
+id int NOT NULL auto_increment,
+nome varchar (30) NOT NULL,
+nascimento date ,
+sexo ENUM ('M' , 'F'),
+peso decimal (5,2),
+altura decimal (3,2),
+nacionalide varchar(20) DEFAULT 'Brasil',
+primary key (id)
+
+)default charset utf8;
