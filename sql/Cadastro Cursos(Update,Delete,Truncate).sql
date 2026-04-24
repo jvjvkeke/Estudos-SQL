3293fa2426e895220600e627ec9d3ee0aa08350e
Joao VItor Vieira de Souza <41848897+jvjvkeke@users.noreply.github.com>
2026-04-23 21:09:40 -0300

    Cadastro_Cursos

--- sql/Cadastro Cursos(Update,Delete,Truncate).sql
+++ sql/Cadastro Cursos(Update,Delete,Truncate).sql
@@ -1,8 +1,6 @@
--- VISUALIZAÇÃO
 SELECT * FROM cadastro_cursos;
 
--- INSERÇÃO DE DADOS
-/*
+
 INSERT INTO cadastro_cursos VALUES 
 (1,'HTML4','Curso de HTML5',40,37,2014),
 (2,'Algoritmos','Logica de Programação',20,15,2014),
@@ -14,18 +12,14 @@ INSERT INTO cadastro_cursos VALUES
 (8,'Sapateado','Danças Ritmicas',40,30,2018),
 (9,'Cozinha Árabe','Aprenda a fazer Kibe',40,30,2018),
 (10,'Youtuber','Gerar Polemicas e ganhar inscritos',5,2,2018);
-*/
 
--- =========================
--- ATUALIZAÇÃO
--- =========================
--- UPDATE cadastro_cursos SET nome = 'HTML5' WHERE id_curso = 1;
--- UPDATE cadastro_cursos SET nome = 'PHP', ano = 2014 WHERE id_curso = 4;
--- UPDATE cadastro_cursos SET nome = 'Java', carga = 40, ano = 2015 WHERE id_curso = 5 LIMIT 1;
 
--- =========================
--- DELEÇÃO
--- =========================
+
+ UPDATE cadastro_cursos SET nome = 'HTML5' WHERE id_curso = 1;
+ UPDATE cadastro_cursos SET nome = 'PHP', ano = 2014 WHERE id_curso = 4;
+ UPDATE cadastro_cursos SET nome = 'Java', carga = 40, ano = 2015 WHERE id_curso = 5 LIMIT 1;
+
+
 DELETE FROM cadastro_cursos WHERE id_curso = 8;
 DELETE FROM cadastro_cursos WHERE id_curso = 9;
 DELETE FROM cadastro_cursos WHERE id_curso = 10;
